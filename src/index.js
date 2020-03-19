f4861330819809c4ea1b56a2abdb1725ef365f1c
tanyushanel <tanyushanel@users.noreply.github.com>
2020-03-19 20:52:18 +0300

    feat: solution added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let absValue = Math.abs(n);
+    let toArr = absValue.toString().split('');
+    let reverse = toArr.reverse();
+    return reverse.join('');
 }
